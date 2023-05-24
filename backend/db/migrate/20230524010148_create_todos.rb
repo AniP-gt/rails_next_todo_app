2d44b76e77681367def7617b86cb3c545a740f74
AniP-gt <takuya@Takuya-MacBookAir.local>
2023-05-24 10:14:29 +0900

    fix: migrationファイルの修正

--- backend/db/migrate/20230524010148_create_todos.rb
+++ backend/db/migrate/20230524010148_create_todos.rb
@@ -1,9 +1,9 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
-    create_table :todos do |t|
+    create_table :todos, comment: 'ToDoテーブル' do |t|
       t.string :title, comment: 'タイトル'
       t.text :content, comment: ' タスクの詳細な説明や内容'
-      t.boolean :status, comment: 'タスクの状態'
+      t.boolean :status, default: false, comment: 'タスクの状態'
       t.string :priority, comment: 'タスクの優先度'
       t.date :due_date, comment: 'タスクの期限日'
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_24_010148) do
-  create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", comment: "ToDoテーブル", force: :cascade do |t|
     t.string "title", comment: "タイトル"
     t.text "content", comment: " タスクの詳細な説明や内容"
-    t.boolean "status", comment: "タスクの状態"
+    t.boolean "status", default: false, comment: "タスクの状態"
     t.string "priority", comment: "タスクの優先度"
     t.date "due_date", comment: "タスクの期限日"
     t.datetime "created_at", null: false
